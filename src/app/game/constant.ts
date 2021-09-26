f71a6a4fc50a16f85b680a00f6fbca95b6e5cf0d
Pratik Sharma <pratiksharma@live.in>
2021-09-26 21:43:17 +0530

    game page completed

---
+++ src/app/game/constant.ts
@@ -0,0 +1,11 @@
+
+
+export const CHOICES = ["rock", "paper", "scissors", "spock", "lizard"];
+
+export const COMBINATIONS = {
+  lizard    : ['spock'    ,'paper'   ],
+  paper     : ['rock'     ,'spock'   ],
+  rock      : ['scissors' ,'lizards' ],
+  scissors  : ['paper'    ,'lizard'  ],
+  spock     : ['rock'     ,'scissors']
+}
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CHOICES, COMBINATIONS } from './constant';
 
 @Component({
   selector: 'rps-game',
@@ -13,25 +14,45 @@ export class GameComponent implements OnInit {
   public counter: number = 3;
   public interval;
   public status: string;
+  public result: string;
 
   constructor(private route: ActivatedRoute) { }
 
   public ngOnInit(): void {
     this.getMyChoice();
     this.startCounter();
-
+    this.getHouseChoice();
   }
 
-  public getMyChoice(): void {
+  private getHouseChoice() {
+    this.houseChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)];
+  }
+
+  private getMyChoice(): void {
     this.myChoice = this.route.snapshot.paramMap.get('myChoice');
   }
 
-  public startCounter() {
+  private startCounter() {
     this.interval = setInterval(() => {
       this.counter -= 1;
       if (this.counter === 0) {
         clearInterval(this.interval);
+        this.findWinner();
       }
     }, 1000);
+  }
+
+  private findWinner() {
+    this.result = 'Draw';
+    
+    if (this.houseChoice === this.myChoice) return;
+
+    if (COMBINATIONS[this.myChoice].includes(this.houseChoice)) {
+      this.status = 'win';
+      this.result = 'You won';
+    } else {
+      this.status = 'lose';
+      this.result = 'You lose';
+    }
   }
 }
